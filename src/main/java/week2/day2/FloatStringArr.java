9c58daf77b7b560cd8f28e36638beb8973281d32
KokoaTree <4kakao1@daum.net>
2023-04-25 14:50:54 +0900

    feat: 참조타입 배열 선언과 null - ReferenceTypeArray 실습 - FloatStringArr 실습

---
+++ src/main/java/week2/day2/FloatStringArr.java
@@ -0,0 +1,13 @@
+package week2.day2;
+
+import java.util.Arrays;
+
+public class FloatStringArr {
+    public static void main(String[] args) {
+        float[] fArr = new float[3];
+        String[] sArr = new String[10];
+
+        System.out.println(Arrays.toString(fArr));
+        System.out.println(Arrays.toString(sArr));
+    }
+}
---
+++ src/main/java/week2/day2/ReferenceTypeArray.java
@@ -0,0 +1,11 @@
+package week2.day2;
+
+import java.util.List;
+
+public class ReferenceTypeArray {
+    public static void main(String[] args) {
+        String[] sArr = new String[3];
+        System.out.println(sArr[0]);
+
+    }
+}
