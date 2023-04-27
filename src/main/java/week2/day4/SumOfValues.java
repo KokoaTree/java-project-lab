662565833a82528b2a4c04c2c9c758fdb343b333
KokoaTree <4kakao1@daum.net>
2023-04-27 09:24:39 +0900

    feat: 배열 모든 값 더하기 - SumOfValues 실습

---
+++ src/main/java/week2/day4/SumOfValues.java
@@ -0,0 +1,15 @@
+package week2.day4;
+
+public class SumOfValues{
+    public static void main(String[]args){
+        int answer = 0;
+        int[] arr = {2, 1, 7, 9};
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.printf("%d", answer);
+//        System.out.println(arr[0] + arr[1] + arr[2] + arr[3]);
+    }
+}
